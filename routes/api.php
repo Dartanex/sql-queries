e617646f3699aec749774d749a5f8eb9b2c92bcf
Josue Orantes <daniel02josue@gmail.com>
2024-06-02 02:37:02 -0600

    :sparkles:feat(migrations): add tables usuarios and pedidos

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Usuarios;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::post('/createUser', [Usuarios::class, 'create']);
+Route::get('/users', [Usuarios::class, 'usuarios']);
+Route::get('usersStartR', [Usuarios::class, 'usuariosNameStartWithR']);
